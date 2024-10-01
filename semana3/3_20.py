73c02078033e45db565474ff8e6a194458881ce4
Isaac Barbosa <isaaccezar22@gmail.com>
2024-10-01 18:30:56 +0100

    Inclui segunda parte da questao 3_20

--- semana3/3_20.py
+++ semana3/3_20.py
@@ -10,3 +10,14 @@ def codificar(cadeia, parametro):
 teste = "aaa"
 print(teste)
 print(codificar(teste, 2))
+codificado = codificar(teste, 2)
+
+def decodificar(cadeia, parametro):
+    for a in cadeia:
+        if(alfabeto.find(a) >= 0):
+            indice = alfabeto.find(a)
+            cadeia = cadeia.replace(a, alfabeto[indice - parametro])
+    return cadeia
+
+print(codificado)
+print(decodificar(codificado, 2))
